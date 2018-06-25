42789ac58cf6daa36baff5a605efd2287772a81e
Kendall <nicho147@gmail.com>
2018-06-25 17:35:26 -0400

    button changes header

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('It works!')
+const button = document.querySelector('button')
+
+const updateText = function(){
+	const header = document.querySelector('h1')
+	header.textContent = 'Chris Pratt is number 1 <3'
+}
+
+button.addEventListener('click',updateText)
