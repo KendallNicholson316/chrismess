7d9ebcabd2b64d3c8d4a5ed3f4808c11ab8084b1
Kendall <nicho147@gmail.com>
2018-06-25 17:52:16 -0400

    button edits second header

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ console.log('It works!')
 const button = document.querySelector('button')
 
 const updateText = function(){
-	const header = document.querySelector('h1')
-	header.textContent = 'Chris Pratt is number 1 <3'
+	const header2 = document.querySelector('h2.header')
+	header2.textContent = 'still is'
 }
 
 button.addEventListener('click',updateText)
