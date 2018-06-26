ba768e871886603884e4795959a127df580065d9
Kendall <nicho147@gmail.com>
2018-06-26 09:40:53 -0400

    readd button for header edit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 console.log('It works!')
 const form = document.getElementById('form1')
+const button = document.querySelector('button')
 
-const updateText = function(event){
+const updateChris = function(event){
 	event.preventDefault()
 	const header1 = document.querySelector('h1.header')
 	header1.textContent = document.getElementById('form1').elements['favChris'].value
 	form.reset()	
 }
 
-form.addEventListener('submit',updateText)
+const updateHeader = function(){
+	const header2 = document.querySelector('h2.header')
+	header2.textContent = 'still is'
+}
+
+button.addEventListener('click',updateHeader)
+form.addEventListener('submit',updateChris)
