b59ccf9bb84e07bd3e300cfb9843e25f2681f28f
Kendall <nicho147@gmail.com>
2018-06-27 17:24:45 -0400

    use ES6 class structure

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const form = document.querySelector('form#movieForm')
+class App{
+	constructor(){
+		const form = document.querySelector('form#movieForm')
+		form.addEventListener('submit', (ev) => {
+			ev.preventDefault()
+			this.updateMovieList(ev)
+		})	
+	}
 
-function createItem(f){
-	const movieName = f.movieName.value
-    const chris = f.chris.value
+
+
+
+	createItem(f){
+		const movieName = f.movieName.value
+    	const chris = f.chris.value
 	     
-    const movieNameSpan = document.createElement('span')
-	movieNameSpan.classList.add('name')
-    movieNameSpan.textContent = movieName
+    	const movieNameSpan = document.createElement('span')
+		movieNameSpan.classList.add('name')
+    	movieNameSpan.textContent = movieName
      
-    const chrisSpan = document.createElement('span')
-	chrisSpan.classList.add('chris')
-    chrisSpan.textContent = ' - ' +  chris
+    	const chrisSpan = document.createElement('span')
+		chrisSpan.classList.add('chris')
+    	chrisSpan.textContent = ' - ' +  chris
  
-    const item = document.createElement('li')
-    item.appendChild(movieNameSpan)
-    item.appendChild(chrisSpan)
+    	const item = document.createElement('li')
+    	item.appendChild(movieNameSpan)
+    	item.appendChild(chrisSpan)
 
-	return item;
+		return item;
+	}
+
+	updateMovieList(ev) {
+//		ev.preventDefault()
+		const f = ev.target
+
+		const list = document.querySelector('#movies')
+		list.appendChild(this.createItem(f))
+
+		f.reset()
+		f.movieName.focus()
+	}
 }
 
-const updateMovieList = function(ev) {
-	ev.preventDefault()
-	const f = ev.target
-
-	const list = document.querySelector('#movies')
-	list.appendChild(createItem(f))
-
-	f.reset()
-	f.movieName.focus()
-}
-
-form.addEventListener('submit', updateMovieList)
+const app = new App()
