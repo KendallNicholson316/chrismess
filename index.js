3d55a86298da211190e03a2bd29eddfba698acdd
Kendall <nicho147@gmail.com>
2018-06-27 18:21:49 -0400

    clear commented out sections

--- index.js
+++ index.js
@@ -23,21 +23,6 @@ class App{
 			const span = this.createSpan(keyName, movie[keyName])
 			item.appendChild(span)
 		})
-/*		const movieName = f.movieName.value
-    	const chris = f.chris.value
-	     
-    	const movieNameSpan = document.createElement('span')
-		movieNameSpan.classList.add('name')
-    	movieNameSpan.textContent = movieName
-     
-    	const chrisSpan = document.createElement('span')
-		chrisSpan.classList.add('chris')
-    	chrisSpan.textContent = ' - ' +  chris
- 
-    	const item = document.createElement('li')
-    	item.appendChild(movieNameSpan)
-    	item.appendChild(chrisSpan)
-*/
 		
 		return item;
 	}
@@ -49,8 +34,6 @@ class App{
 			name: f.movieName.value,
 			chris: f.chris.value,
 		}
-		
-//		const item = this.createItem(movie)
 
 		const list = document.querySelector('#movies')
 		list.appendChild(this.createItem(movie))
