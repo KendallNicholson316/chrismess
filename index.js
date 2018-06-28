b9271fa5d0d3ba5086e0d4233ff9499f91588679
Kendall <nicho147@gmail.com>
2018-06-28 18:08:48 -0400

    refactor button creation

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 class App{
 	constructor(){
+		this.list = document.querySelector('#movies')
 		this.movieList = []
+
+		
 		const form = document.querySelector('form#movieForm')
 		form.addEventListener('submit', (ev) => {
 			ev.preventDefault()
-			this.updateMovieList(ev)
+			this.handleSubmit(ev)
 		})	
 	}
 
@@ -15,7 +18,17 @@ class App{
 	favorite(button){
         button.textContent = '🖤'
     }
+	
+	createUtilities(movie, item){
+		const div = document.createElement('div')
 
+		div.appendChild(this.createRemoveButton(movie, item))		
+	
+		div.appendChild(this.createFavoriteButton())
+		
+		return div;
+	}
+	
     createFavoriteButton(){
 		let i =0;
         const button = document.createElement('button')
@@ -34,45 +47,52 @@ class App{
         return button
     }
 	
-	remove(ev,movie){
-		const item = ev.target.parentElement
+	remove(movie, item){
+		this.list.removeChild(item)
 		const i = this.movieList.indexOf(movie)
         this.movieList.splice(i,1)
-		
-		item.parentElement.removeChild(item)
 	}
 
-	createRemoveButton(movie){
+	createRemoveButton(movie, item){
 		const button = document.createElement('button')
         button.classList.add('close')
         button.textContent ='🗑️'
-		button.addEventListener('click', (ev) => {
-            ev.preventDefault()
-            this.remove(ev,movie)
-        })
+		button.addEventListener('click', () => this.remove(movie,item))
 		return button
 	}
 
 	createSpan(name, value){
-		const span = document.createElement('span')
+		const span = document.createElement('li')
 		span.classList.add(name)
-		span.textContent = value		
+		span.textContent = value
 		return span
 	}
+
+	createProps(movie, item){
+		const div = document.createElement('div')
+		const keys = Object.keys(movie)
+
+        keys.forEach((keyName) => {
+            div.appendChild(this.createSpan(keyName, movie[keyName]))
+        })
+		
+		return div		
+	}
+
 	createItem(movie){
 		const item = document.createElement('li')
 		item.classList.add('movie')
-
-		const keys = Object.keys(movie)
 		
-		keys.forEach((keyName) => {	
-			item.appendChild(this.createSpan(keyName, movie[keyName]))
-		})	
+		const props = this.createProps(movie, item)
+		item.appendChild(props)
+
+		const utilities = this.createUtilities(movie, item)
+		item.appendChild(utilities)
 		
 		return item;
 	}
 
-	updateMovieList(ev) {	
+	handleSubmit(ev) {	
 		const f = ev.target
 		
 		const movie = {
@@ -82,11 +102,8 @@ class App{
 
 		this.movieList.push(movie)
 
-		const list = document.querySelector('#movies')
 		const item = this.createItem(movie)
-		item.appendChild(this.createRemoveButton(movie))
-		item.appendChild(this.createFavoriteButton())
-		list.appendChild(item)
+		this.list.appendChild(item)
 		
 		f.reset()
 		f.movieName.focus()
